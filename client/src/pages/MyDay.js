8eba82f0f6b28d26216c589a69d6105410898a41
oliviaguerra731@gmail.com <102640672+nightbird99@users.noreply.github.com>
2022-10-13 18:05:09 -0500

    fixed import

--- client/src/pages/MyDay.js
+++ client/src/pages/MyDay.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import { Navigate } from 'react-router-dom';
 import { Container, Col, Row, Form, Button, Card, CardColumns, Modal } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import { useQuery, useMutation } from '@apollo/client';
