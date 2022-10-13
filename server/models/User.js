246f58e8852ed3ad10589b9e5e1285bfeac4ae4c
oliviaguerra731@gmail.com <102640672+nightbird99@users.noreply.github.com>
2022-10-12 20:21:49 -0500

    correcting deployment issues

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const eventSchema = require('./Event');
+const eventSchema = require('./Event').eventSchema;
 
 const userSchema = new Schema(
     {
