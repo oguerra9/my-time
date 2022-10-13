add0f2058ec70bce9ffc4919ddc3dec684943a80
oliviaguerra731@gmail.com <102640672+nightbird99@users.noreply.github.com>
2022-10-13 16:57:55 -0500

    updating imports to match new component directories

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
-import SignUpForm from './SignupForm';
-import LoginForm from './LoginForm';
+import SignUpForm from '../SignupForm';
+import LoginForm from '../LoginForm';
 //import AddEventForm from './AddEventForm';
 
 import Auth from '../utils/auth';
